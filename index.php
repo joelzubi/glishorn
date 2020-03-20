6337de4a34495b401234ee6cabdfb87dd90da2cd
joelzubi <joel.zubi@gmail.com>
2020-03-20 13:36:05 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="de-CH">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <title>MG Glishorn</title>
+    <!-- Bootstrap Stylesheet -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+
+<!-- A little experiment -->
+
+<nav class="navbar navbar-expand-sm bg-white navbar-light sticky-top">
+    <a class="navbar-brand" href="#">
+        <img src="src/brand.jpeg" alt="MG Glishorn Logo" style="width: 64px">
+    </a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Info</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<div class="jumbotron bg-danger text-white">
+    <h1>Musikgesellschaft Glishorn Glis</h1>
+</div>
+
+<div class="container-sm">
+    <div class="card" style="max-width: 600px;">
+        <img class="card-img-top" src="src/example_poster_lg.jpg" alt="Konzertflyer">
+        <div class="card-body">
+            <h4 class="card-title">Konzert</h4>
+            <p class="card-text">Lorem ipsum.</p>
+        </div>
+    </div>
+</div>
+
+<!-- Bootstrap Scripts -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
