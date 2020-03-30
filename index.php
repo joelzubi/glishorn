f0640a86b1556319919f3ce238fa08eebcf51032
joelzubi <joel.zubi@gmail.com>
2020-03-30 19:39:13 +0200

    Change to PHP

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php require 'templates.php' ?>
+<!DOCTYPE html>
+<html lang="de-CH">
+<head>
+    <meta charset="UTF-8">
+    <title>MG Glishorn</title>
+    <link rel="icon" href="src/index_images/brand.jpeg" type="image/x-icon">
+
+    <!-- For responsive objects -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap Stylesheet -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+
+<body>
+<?php echo header() ?>
+<?php echo navbar('index') ?>
+<?php echo title('Musikgesellschaft &laquo;Glishorn&raquo; Glis') ?>
+<?php echo unofficial_alert() ?>
+
+<!-- Corona alert -->
+<div id="coronaAlert" class="jumbotron-fluid p-3 my-3 bg-danger text-white">
+    <div class="container">
+        <h4>Coronavirus</h4>
+        <p>Aufgrund der aktuellen Lage wurden die Konzerte der
+            <abbr title="Musikgesellschaft">MG</abbr> Glishorn und
+            <abbr title="Jugendmusik">JM</abbr> Glis  abgesagt. Auch der Probebetrieb
+            und interne Anlässe sind bis mindestens 19. April eingestellt.</p>
+        <p>Das Lotto wird zu einem späteren Zeitpunkt durchgeführt, nach Möglichkeit
+            in den Gliser Restaurants mit Beteiligung der Öffentlichkeit.</p>
+        <a href="#" class="btn btn-secondary">Mehr</a>
+    </div>
+</div>
+
+<?php echo footer() ?>
+
+<!-- Bootstrap Scripts -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
---
+++ templates.php
@@ -0,0 +1,58 @@
+<?php
+    function header() {
+        return '<div class="jumbotron-fluid top-image"></div>';
+    }
+
+    function navbar($current_page) {
+        $navbar =
+            '<nav class="navbar navbar-expand-sm bg-light navbar-light sticky-top">
+                <a class="navbar-brand" href="#">
+                    <img src="src/index_images/brand.jpeg" alt="MG Glishorn Logo" style="width: 64px">
+                </a>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                    <ul class="navbar-nav">';
+
+        if ($current_page == 'index') {
+            $navbar .= '<li class="nav-item active">
+                            <a class="nav-link" href="#">Home</a>
+                        </li>';
+        } else {
+            $navbar .= '<li class="nav-item">
+                            <a class="nav-link" href="index.php">Home</a>
+                        </li>';
+        }
+
+        $navbar .= '</ul>
+                </div>
+            </nav>';
+
+        return $navbar;
+    }
+
+    function title($title) {
+        return "<div class='container'>
+                    <h1>{$title}</h1>
+                </div>";
+    }
+
+    function unofficial_alert() {
+        return '<div class="container">
+                    <div class="alert alert-warning">
+                        Dies ist nicht die offizielle Website der MG Glishorn.
+                        <a href="https://www.glishorn.ch/" class="alert-link">
+                            Zur offiziellen Website.
+                        </a>
+                    </div>
+                </div>';
+    }
+
+    function footer() {
+        return '<footer class="container-fluid p-3 my-3 text-center bg-light">
+                    <address>3902 Glis</address>
+                </footer>';
+    }
