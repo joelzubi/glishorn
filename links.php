34072ace7642c93a6440cef03d106a3521bdf419
joelzubi <joel.zubi@gmail.com>
2020-04-01 01:39:54 +0200

    refactoring

--- links.php
+++ links.php
@@ -12,8 +12,9 @@
     <!-- Bootstrap Stylesheet -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
 
-    <!-- Custom Stylesheet -->
+    <!-- Custom Stylesheets -->
     <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/links.css">
 </head>
 
 <body>
@@ -29,67 +30,65 @@
     <h4>Musikgesellschaften</h4>
     <div class="row">
         <div class="col-md-4 my-1">
-
-                <a href="http://mgbryscheralp.ch/wp/" class="btn btn-outline-primary">
-                    <img src="src/bands/mund.jpg" alt="MG Bryscheralp" style="width: 100%;">
-                    <div class="caption">
-                        MG &laquo;Bryscheralp&raquo; Mund
-                    </div>
-                </a>
-
+            <a href="http://mgbryscheralp.ch/wp/" class="btn btn-outline-primary">
+                <img src="src/bands/mund.jpg" alt="MG Bryscheralp" class="link-image">
+                <div class="caption">
+                    MG &laquo;Bryscheralp&raquo; Mund
+                </div>
+            </a>
         </div>
         <div class="col-md-4 my-1">
-                <a href="https://www.mg-belalp.ch/" class="btn btn-outline-primary">
-                    <img src="src/bands/naters.jpg" alt="MG Belalp" style="width: 100%">
-                    <div class="caption">
-                        MG &laquo;Belalp&raquo; Naters
-                    </div>
-                </a>
+            <a href="https://www.mg-belalp.ch/" class="btn btn-outline-primary">
+                <img src="src/bands/naters.jpg" alt="MG Belalp" class="link-image">
+                <div class="caption">
+                    MG &laquo;Belalp&raquo; Naters
+                </div>
+            </a>
         </div>
         <div class="col-md-4 my-1">
-                <a href="https://www.stadtmusik-saltina.ch/" class="btn btn-outline-primary">
-                    <img src="src/bands/brig.jpg" alt="Stadtmusik Saltina" style="width: 100%;">
-                    <div class="caption">
-                        BO Stadtmusik &laquo;Saltina&raquo; Brig
-                    </div>
-                </a>
+            <a href="https://www.stadtmusik-saltina.ch/" class="btn btn-outline-primary">
+                <img src="src/bands/brig.jpg" alt="Stadtmusik Saltina" class="link-image">
+                <div class="caption">
+                    BO Stadtmusik &laquo;Saltina&raquo; Brig
+                </div>
+            </a>
         </div>
     </div>
     <div class="row">
         <div class="col-md-4 my-1">
-                <a href="https://www.mgsaflischtermen.com/" class="btn btn-outline-primary">
-                    <img src="src/bands/termen.jpg" alt="MG Saflisch" style="width: 100%;">
-                    <div class="caption">
-                        MG &laquo;Saflisch&raquo; Termen
-                    </div>
-                </a>
+            <a href="https://www.mgsaflischtermen.com/" class="btn btn-outline-primary">
+                <img src="src/bands/termen.jpg" alt="MG Saflisch" class="link-image"">
+                <div class="caption">
+                    MG &laquo;Saflisch&raquo; Termen
+                </div>
+            </a>
         </div>
         <div class="col-md-4 my-1">
-                <a href="https://www.1815.ch/rhonezeitung/zeitung/region/die-gesellige-musikgesellschaft-aus-eggerberg-90867/"
-                   class="btn btn-outline-primary">
-                    <img src="src/bands/eggerberg.jpg" alt="MG Findleralp" style="width: 100%;">
-                    <div class="caption">
-                        MG &laquo;Findleralp&raquo; Eggerberg
-                    </div>
-                </a>
+            <a href="https://www.1815.ch/rhonezeitung/zeitung/region/die-gesellige-musikgesellschaft-aus-eggerberg-90867/"
+               class="btn btn-outline-primary">
+                <img src="src/bands/eggerberg.jpg" alt="MG Findleralp" class="link-image">
+                <div class="caption">
+                    MG &laquo;Findleralp&raquo; Eggerberg
+                </div>
+            </a>
         </div>
         <div class="col-md-4 my-1">
-                <a href="https://www.mgeintracht.ch/" class="btn btn-outline-primary">
-                    <img src="src/bands/moerel-filet.jpg" alt="MG Eintracht" style="width: 100%;">
-                    <div class="caption">
-                        MG &laquo;Eintracht&raquo; Mörel-Filet
-                    </div>
-                </a>
+            <a href="https://www.mgeintracht.ch/" class="btn btn-outline-primary">
+                <img src="src/bands/moerel-filet.jpg" alt="MG Eintracht" class="link-image">
+                <div class="caption">
+                    MG &laquo;Eintracht&raquo; Mörel-Filet
+                </div>
+            </a>
         </div>
     </div>
     <div class="row">
         <div class="col-md-4 my-1">
-                <a href="http://www.mg-simplon.ch/" class="btn btn-outline-primary">
-                    <img src="src/bands/ried-brig.jpg" alt="MG Simplon" style="width: 100%;">
-                    <div class="caption">
-                        MG &laquo;Simplon&raquo; Ried-Brig
-                    </div>
-                </a>
+            <a href="http://www.mg-simplon.ch/" class="btn btn-outline-primary">
+                <img src="src/bands/ried-brig.jpg" alt="MG Simplon" class="link-image">
+                <div class="caption">
+                    MG &laquo;Simplon&raquo; Ried-Brig
+                </div>
+            </a>
         </div>
     </div>
 </div>
@@ -100,7 +99,7 @@
     <div class="row">
         <div class="col-md-4 my-1">
                 <a href="http://www.jugendmusik-glis.ch/" class="btn btn-outline-primary">
-                    <img src="src/bands/jm_glis.jpg" alt="JM Glis" style="width: 100%;">
+                    <img src="src/bands/jm_glis.jpg" alt="JM Glis" class="link-image">
                     <div class="caption">
                         Jugendmusik Glis
                     </div>
@@ -108,7 +107,7 @@
         </div>
         <div class="col-md-4 my-1">
                 <a href="https://www.jm-brig.ch/" class="btn btn-outline-primary">
-                    <img src="src/bands/jm_brig.jpeg" alt="JM Brig" style="width: 100%;">
+                    <img src="src/bands/jm_brig.jpeg" alt="JM Brig" class="link-image">
                     <div class="caption">
                         Jugendmusik Brig
                     </div>
@@ -117,7 +116,7 @@
         <div class="col-md-4 my-1">
                 <a href="https://www.1815.ch/rhonezeitung/zeitung/region/erfolgreiche-nachwuchsarbeit/"
                    class="btn btn-outline-primary">
-                    <img src="src/bands/jm_naters.jpg" alt="JM Belalp" style="width: 100%">
+                    <img src="src/bands/jm_naters.jpg" alt="JM Belalp" class="link-image">
                     <div class="caption">
                         Jugendmusik &laquo;Belalp&raquo; Naters
                     </div>
@@ -132,7 +131,7 @@
     <div class="row">
         <div class="col-md-4 my-1">
                 <a href="https://www.soundonstreet.ch/" class="btn btn-outline-primary">
-                    <img src="src/bands/sos.jpg" alt="SOS" style="width: 100%;">
+                    <img src="src/bands/sos.jpg" alt="SOS" class="link-image">
                     <div class="caption">
                         Sound on Street (SOS)
                     </div>
@@ -140,7 +139,7 @@
         </div>
         <div class="col-md-4 my-1">
                 <a href="http://www.tonymalonys.ch/" class="btn btn-outline-primary">
-                    <img src="src/bands/malonys.jpg" alt="Tony Malony's" style="width: 100%;">
+                    <img src="src/bands/malonys.jpg" alt="Tony Malony's" class="link-image">
                     <div class="caption">
                         Tony Malony's
                     </div>
@@ -148,7 +147,7 @@
         </div>
         <div class="col-md-4 my-1">
                 <a href="https://www.modernwheatbeat.com/" class="btn btn-outline-primary">
-                    <img src="src/bands/modern_wheat_beat.jpg" alt="Modern Wheat Beat" style="width: 100%">
+                    <img src="src/bands/modern_wheat_beat.jpg" alt="Modern Wheat Beat" class="link-image">
                     <div class="caption">
                         Modern Wheat Beat
                     </div>
@@ -164,7 +163,7 @@
         <div class="col-md-4 my-1">
             <div class="thumbnail">
                 <a href="https://www.windband.ch/de/home/" class="btn btn-outline-primary">
-                    <img src="src/bands/sbv.png" alt="SBV" style="width: 100%;">
+                    <img src="src/bands/sbv.png" alt="SBV" class="link-image">
                     <div class="caption">
                         Schweizer Blasmusikverband
                     </div>
@@ -174,7 +173,7 @@
         <div class="col-md-4 my-1">
             <div class="thumbnail">
                 <a href="https://www.kmvw.ch/" class="btn btn-outline-primary">
-                    <img src="src/bands/kmvw.png" alt="KMVW" style="width: 100%;">
+                    <img src="src/bands/kmvw.png" alt="KMVW" class="link-image">
                     <div class="caption">
                         Kantonaler Musikverband Wallis
                     </div>
@@ -184,7 +183,7 @@
         <div class="col-md-4 my-1">
             <div class="thumbnail">
                 <a href="https://www.omv-vs.ch/" class="btn btn-outline-primary">
-                    <img src="src/bands/omv.png" alt="OMV" style="width: 100%">
+                    <img src="src/bands/omv.png" alt="OMV" class="link-image">
                     <div class="caption">
                         Oberwalliser Musikverband
                     </div>
@@ -196,7 +195,7 @@
         <div class="col-md-4 my-1">
             <div class="thumbnail">
                 <a href="https://www.jugendmusik.ch/de" class="btn btn-outline-primary">
-                    <img src="src/bands/jm_ch.jpg" alt="Jugendmusik" style="width: 100%;">
+                    <img src="src/bands/jm_ch.jpg" alt="Jugendmusik" class="link-image">
                     <div class="caption">
                         Schweizer Jugendmusikverband
                     </div>
@@ -213,7 +212,7 @@
         <div class="col-md-4 my-1">
             <div class="thumbnail">
                 <a href="https://www.amovisp.ch/" class="btn btn-outline-primary">
-                    <img src="src/bands/amo.png" alt="AMO" style="width: 100%;">
+                    <img src="src/bands/amo.png" alt="AMO" class="link-image">
                     <div class="caption">
                         Allgemeine Musikschule Oberwallis
                     </div>
