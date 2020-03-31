57f79e49ec57107d65437813af32a4c23db9a0e8
joelzubi <joel.zubi@gmail.com>
2020-03-31 18:44:28 +0200

    add empty article

---
+++ articles/corona.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1); require '../templates.php' ?>
+<!DOCTYPE html>
+<html lang="de-CH">
+<head>
+    <meta charset="UTF-8">
+    <title>Corona | MG Glishorn</title>
+    <link rel="icon" href="../src/index_images/brand.jpeg" type="image/x-icon">
+
+    <!-- For responsive objects -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap Stylesheet -->
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+
+    <!-- Custom Stylesheet -->
+    <link rel="stylesheet" href="../css/header.css">
+</head>
+
+<body>
+<?php echo topImage() ?>
+<?php echo navbar('corona') ?>
+<?php echo title('COVID-19') ?>
+<?php echo unofficial_alert() ?>
+
+<!-- Content goes here -->
+
+<?php echo footer() ?>
+
+<!-- Bootstrap Scripts -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+
+</body>
