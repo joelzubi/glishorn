df44d79f1bfe3a0ad52992eb489ff2f26f8abcd2
joelzubi <joel.zubi@gmail.com>
2020-04-01 14:14:28 +0200

    try something

--- lists/board.php
+++ lists/board.php
@@ -1,43 +1,57 @@
 <?php
-$sql = "SELECT CASE WHEN
-    Person.female = 1 THEN BoardPosition.title_female ELSE BoardPosition.title_male
-END AS function,
-CONCAT(
-    Person.firstname,
-    ' ',
-    Person.lastname
-) AS name
+$sql = "SELECT
+    CONCAT(
+        Person.firstname,
+        ' ',
+        Person.lastname
+    ) AS name
 FROM
     (
         Person
-    INNER JOIN Member ON Person.pid = Member.pid
+    INNER JOIN(
+            Musician
+        INNER JOIN(
+                Instrument
+            INNER JOIN Section ON Instrument.sid = Section.sid
+            )
+        ON
+            Musician.iid = Instrument.iid
+        )
+    ON
+        Person.pid = Musician.pid
     )
-INNER JOIN(
-        BoardMember
-    INNER JOIN BoardPosition ON BoardMember.bid = BoardPosition.bid
-    )
-ON
-    Person.pid = BoardMember.pid";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    echo '  <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <th>Funktion</th>
-                        <th>Name</th>
-                    </tr>
-                </thead>
-                <tbody>';
+WHERE
+    Section.plural = ?";
 
-    while($row = $result->fetch_assoc()) {
-        echo "      <tr>
-                        <td>{$row['function']}</td>
-                        <td>{$row['name']}</td>
-                    </tr>";
+$singular_sql = "SELECT
+    singular
+FROM
+    Section
+WHERE
+    plural = ?";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $section);
+
+
+$section = "Klarinetten";
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+
+
+    echo "  <div class='container'>
+                            <h3>$section</h3>
+                            <ul class='list-group list-group-flush'>";
+    while ($row = $result->fetch_assoc()) {
+        echo "      <li class='list-group-item'>{$row["name"]}</li>";
     }
 
-    echo '     </tbody>
-            </table>';
+    echo '      </ul>
+                        </div>';
 } else {
     echo "Keine Resultate";
 }
+
+$stmt->close();
