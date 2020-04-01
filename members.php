2803dafd1266d691fe8cfdf79038d7cacff3f63f
joelzubi <joel.zubi@gmail.com>
2020-04-01 11:02:07 +0200

    add tabs

--- members.php
+++ members.php
@@ -24,21 +24,35 @@
 </div>
 <?php include 'templates/unofficial_alert.php'?>
 
-<!-- Active members -->
 <div class="container">
-<?php
-$servername = "localhost:3306";
-$username = "guest";
-$password = "guest";
-$dbname = "mgglishorn";
+    <ul class="nav nav-pills" role="tablist">
+        <li class="nav-item">
+            <a class="nav-link active" data-toggle="pill" href="#activeTable">Aktivmitglieder</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="pill" href="#boardTable">Vorstand</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="pill" href="#conductorTable">Direktion</a>
+        </li>
+    </ul>
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Verbindungsfehler");
-}
-$conn->set_charset("utf8");
+    <div class="tab-content">
+        <!-- Active members -->
+        <div id="activeTable" class="container tab-pane active">
+            <?php
+            $servername = "localhost:3306";
+            $username = "guest";
+            $password = "guest";
+            $dbname = "mgglishorn";
 
-$sql = "SELECT
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            if ($conn->connect_error) {
+                die("Verbindungsfehler");
+            }
+            $conn->set_charset("utf8");
+
+            $sql = "SELECT
     CONCAT(firstname, ' ', lastname) AS name,
     CASE WHEN Person.pid IN(
 SELECT
@@ -88,9 +102,9 @@ LEFT JOIN(
     )
 ON
     Person.pid = Conductor.pid";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    echo '  <table class="table table-hover">
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0) {
+                echo '  <table class="table table-hover">
                 <thead>
                     <tr>
                         <th>Name</th>
@@ -99,21 +113,31 @@ if ($result->num_rows > 0) {
                 </thead>
                 <tbody>';
 
-    while($row = $result->fetch_assoc()) {
-        echo "      <tr>
+                while($row = $result->fetch_assoc()) {
+                    echo "      <tr>
                         <td>{$row['name']}</td>
                         <td>{$row['function']}</td>
                     </tr>";
-    }
+                }
 
-    echo '     </tbody>
+                echo '     </tbody>
             </table>';
-} else {
-    echo "Keine Resultate";
-}
+            } else {
+                echo "Keine Resultate";
+            }
 
-$conn->close();
-?>
+            $conn->close();
+            ?>
+        </div>
+
+        <div id="boardTable" class="container tab-pane fade">
+            <p>boardTable</p>
+        </div>
+
+        <div id="conductorTable" class="container tab-pane fade">
+            <p>conductorTable</p>
+        </div>
+    </div>
 </div>
 
 <?php include 'templates/footer.php'?>
