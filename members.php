97510be0409d0d379bb8c8a28e23a038c5d35615
joelzubi <joel.zubi@gmail.com>
2020-04-01 13:40:57 +0200

    add sections

--- members.php
+++ members.php
@@ -30,6 +30,9 @@
             <a class="nav-link active" data-toggle="pill" href="#activeTable">Aktivmitglieder</a>
         </li>
         <li class="nav-item">
+            <a class="nav-link" data-toggle="pill" href="#sectionTable">Nach Registern</a>
+        </li>
+        <li class="nav-item">
             <a class="nav-link" data-toggle="pill" href="#boardTable">Vorstand</a>
         </li>
         <li class="nav-item">
@@ -126,7 +129,81 @@ ON
                 echo "Keine Resultate";
             }
 
+            $stmt->close();
+            $singular_stmt->close();
             $conn->close();
+            ?>
+        </div>
+
+        <div id="sectionTable" class="container tap-pane fade">
+            <?php
+            $servername = "localhost:3306";
+            $username = "guest";
+            $password = "guest";
+            $dbname = "mgglishorn";
+
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            if ($conn->connect_error) {
+                die("Verbindungsfehler");
+            }
+            $conn->set_charset("utf8");
+
+            $sql = "SELECT
+    CONCAT(
+        Person.firstname,
+        ' ',
+        Person.lastname
+    ) AS name
+FROM
+    (
+        Person
+    INNER JOIN(
+            Musician
+        INNER JOIN(
+                Instrument
+            INNER JOIN Section ON Instrument.sid = Section.sid
+            )
+        ON
+            Musician.iid = Instrument.iid
+        )
+    ON
+        Person.pid = Musician.pid
+    )
+WHERE
+    Section.plural = ?";
+
+            $singular_sql = "SELECT
+    singular
+FROM
+    Section
+WHERE
+    plural = ?";
+
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("s", $section);
+            $singular_stmt = $conn->prepare($singular_sql);
+            $singular_stmt->bind_param("s", $section);
+
+            $section = "Klarinetten";
+            $stmt->execute();
+            $result = $stmt->get_result();
+
+            if ($result->num_rows > 0) {
+                if ($result->num_rows = 1) {
+                    $singular_stmt->execute();
+                    $section = $singular_stmt->get_result()->fetch_assoc()["singular"];
+                }
+
+                echo "  <div class='container'>
+                            <h3>$section</h3>
+                            <ul class='list-group list-group-flush'>";
+                while ($row = $result->fetch_assoc()) {
+                    echo "      <li class='list-group-item'>$section</li>";
+                }
+
+                echo '      </ul>
+                        </div>';
+            }
             ?>
         </div>
 
