45421250a0030e6c07e1367bf4b0bb58009e9c6c
joelzubi <joel.zubi@gmail.com>
2020-04-25 14:42:25 +0200

    change to Bootstrap CDN

--- photos.php
+++ photos.php
@@ -6,14 +6,19 @@
     <title>Fotos | MG Glishorn</title>
     <link rel="icon" href="src/index_images/brand.jpeg" type="image/x-icon">
 
-    <!-- For responsive objects -->
+    <!-- For responsive design -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Bootstrap Stylesheet -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 
     <!-- Custom Stylesheet -->
     <link rel="stylesheet" href="css/header.css">
+
+    <!-- jQuery, Popper JS and Bootstrap Scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 </head>
 
 <body>
@@ -27,11 +32,5 @@
 <!-- Content goes here -->
 
 <?php include 'templates/footer.php'?>
-
-<!-- Bootstrap Scripts -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-
 </body>
 </html>
