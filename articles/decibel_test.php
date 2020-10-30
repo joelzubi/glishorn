f485e43dc98fc7075352a82ca6c4a1591339212c
joelzubi <joel.zubi@gmail.com>
2020-10-30 13:57:05 +0100

    Add decibel test article.

---
+++ articles/decibel_test.php
@@ -0,0 +1,240 @@
+<?php declare(strict_types=1)?>
+<!DOCTYPE html>
+<html lang="de-CH">
+<head>
+    <meta charset="UTF-8">
+    <title>Dezibel-Test | MG Glishorn</title>
+    <link rel="icon" href="../src/index_images/brand.jpeg" type="image/x-icon">
+
+    <!-- For responsive design -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap Stylesheet -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+    <!-- Custom Stylesheets -->
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/footer.css">
+
+    <!-- jQuery, Popper JS and Bootstrap Scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+    <!-- Icon Script -->
+    <script src="https://kit.fontawesome.com/f54a4f4510.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+<?php include '../templates/header.php'?>
+<?php include '../templates/navbar_article.php' ?>
+<?php include '../templates/unofficial_alert.php'?>
+
+<!-- Article -->
+<div class="container">
+    <h1>Wie laut ist ein Orchester?</h1>
+
+    <section>
+        <p>Täglich benutzen wir unseren Gehörsinn, sei es beim Zuhören in Gesprächen, beim Fernsehen oder im
+            Strassenverkehr.
+            Der Gehörsinn ist der Sinn, der bei den meisten Menschen morgens als Erster aktiviert wird.
+            Ohne einen Wecker schaffen es viele gar nicht erst aus dem Bett.</p>
+        <p>Die Lautstärke von Geräuschen kann, zum Beispiel mit einer App, in Dezibel (dB) gemessen werden.
+            0 dB bedeutet, dass etwas so leise ist, dass es ein Mensch nicht hören kann.
+            Umso höher die Lautstärke ist, desto grösser ist auch die Gefahr fürs menschliche Gehör.
+            Sehr lauten Geräuschen sollte man nicht zu lange ausgesetzt sein, da das Gehör sonst Schaden nimmt.</p>
+        <p>Im folgenden Schema sind beispielhaft einige Geräusche und deren Lautstärke aufgelistet:</p>
+
+        <table class="table">
+            <tbody>
+                <tr class="table-success">
+                    <td>10 dB</td>
+                    <td>Blätterrauschen</td>
+                </tr>
+                <tr class="table-success">
+                    <td>20 dB</td>
+                    <td>Tropfender Wasserhahn</td>
+                </tr>
+                <tr class="table-success">
+                    <td>30 dB</td>
+                    <td>Kühlschrankbrummen</td>
+                </tr>
+                <tr class="table-success">
+                    <td>33 dB</td>
+                    <td>Laufgeräusch eines Videorecorders</td>
+                </tr>
+                <tr class="table-success">
+                    <td>35 dB</td>
+                    <td>Zimmerventilator</td>
+                </tr>
+                <tr class="table-info">
+                    <td>37 dB</td>
+                    <td>Auf den Tisch fallendes Streichholz / Flüstern</td>
+                </tr>
+                <tr class="table-info">
+                    <td>55 dB</td>
+                    <td>Radio / Fernseher</td>
+                </tr>
+                <tr class="table-info">
+                    <td>50-60 dB</td>
+                    <td>Normales Gespräch</td>
+                </tr>
+                <tr class="table-secondary">
+                    <td>65 dB</td>
+                    <td>Angeregtes Gespräch</td>
+                </tr>
+                <tr class="table-secondary">
+                    <td>70 dB</td>
+                    <td>Staubsauger</td>
+                </tr>
+                <tr class="table-secondary">
+                    <td>75 dB</td>
+                    <td>Fahrradklingel</td>
+                </tr>
+                <tr class="table-secondary">
+                    <td>80 dB</td>
+                    <td>Spielende Kinder / Strassenlärm</td>
+                </tr>
+                <tr class="table-warning">
+                    <td>83 dB</td>
+                    <td>Föhn / Vielbefahrene Strasse</td>
+                </tr>
+                <tr class="table-warning">
+                    <td>90 dB</td>
+                    <td>Schnarchen / Fahrender LKW Quietschentchen am Ohr</td>
+                </tr>
+                <tr class="table-warning">
+                    <td>100 dB</td>
+                    <td>Musik in der Disco / Presslufthammer / Kreissäge</td>
+                </tr>
+                <tr class="table-danger">
+                    <td>110-140 dB</td>
+                    <td>Startendes Flugzeug</td>
+                </tr>
+                <tr class="table-danger">
+                    <td>140 dB</td>
+                    <td>Startende Rakete</td>
+                </tr>
+                <tr class="table-danger">
+                    <td>160 dB</td>
+                    <td>Gewehrschuss in Mündungsnähe</td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+
+    <section>
+        <p>Wie laut ist aber ein Instrument? Oder gar ein ganzes Orchester?
+            Dieser Frage gehen wir hier ein für alle Male auf den Grund.</p>
+        <p>In Zusammenarbeit mit verschiedenen Mitgliedern der MG Glishorn und der Jugendmusik Glis haben wir ein
+            kleines Experiment durchgeführt.
+            Der Auftrag an die Musikantinnen und Musikanten war einfach:
+            Sie sollten so leise wie möglich, so laut wie möglich und in einer angenehmen Lautstärke spielen und
+            dabei jeweils die Lautstärke in Dezibel messen.
+            Zusätzlich wurde die Lautstärke in einem Orchester-Stück an verschiedenen Stellen gemessen.</p>
+        <p>Die Ergebnisse sind teilweise erstaunlich und wohl nicht so ganz wissenschaftlich korrekt erfasst.
+            Trotzdem gibt die folgende Tabelle einen interessanten Einblick in die Lautstärke der verschiedenen
+            Instrumente:</p>
+
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>Laut</th>
+                    <th>Leise</th>
+                    <th>Normal</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Oboe</td>
+                    <td>78.2 dB</td>
+                    <td>93.9 dB</td>
+                    <td>88.5 dB</td>
+                </tr>
+                <tr>
+                    <td>Flöte</td>
+                    <td>44.1 dB</td>
+                    <td>80.3 dB</td>
+                    <td>63.2 dB</td>
+                </tr>
+                <tr>
+                    <td>Piccolo</td>
+                    <td>47.0 dB</td>
+                    <td>90.0 dB</td>
+                    <td>80.4 dB</td>
+                </tr>
+                <tr>
+                    <td>Klarinette</td>
+                    <td>34.1 dB</td>
+                    <td>119.3 dB</td>
+                    <td>79.8 dB</td>
+                </tr>
+                <tr>
+                    <td>Waldhorn</td>
+                    <td>69.4 dB</td>
+                    <td>86.4 dB</td>
+                    <td>77.5 dB</td>
+                </tr>
+                <tr>
+                    <td>Trompete</td>
+                    <td>73.8 dB</td>
+                    <td>106.3 dB</td>
+                    <td>91.7 dB</td>
+                </tr>
+                <tr>
+                    <td>Posaune</td>
+                    <td>68.9 dB</td>
+                    <td>87.0 dB</td>
+                    <td>78.9 dB</td>
+                </tr>
+                <tr>
+                    <td>Fagott</td>
+                    <td>79.6 dB</td>
+                    <td>117.0 dB</td>
+                    <td>81.2 dB</td>
+                </tr>
+                <tr>
+                    <td>Englischhorn</td>
+                    <td>80.2 dB</td>
+                    <td>94.1 dB</td>
+                    <td>88.2 dB</td>
+                </tr>
+                <tr>
+                    <td>Xylofon</td>
+                    <td>66.7 dB</td>
+                    <td>98.4 dB</td>
+                    <td>84.0 dB</td>
+                </tr>
+                <tr>
+                    <td>Glockenspiel</td>
+                    <td>77.2 dB</td>
+                    <td>97.7 dB</td>
+                    <td>86.8 dB</td>
+                </tr>
+                <tr>
+                    <td>Drumset</td>
+                    <td>96.0 dB</td>
+                    <td>109.3 dB</td>
+                    <td>101.1 dB</td>
+                </tr>
+                <tr>
+                    <td>Ganzes Orchester</td>
+                    <td>76.5 dB</td>
+                    <td>102.5 dB</td>
+                    <td>94.5 dB</td>
+                </tr>
+            </tbody>
+        </table>
+
+        <p>Die MG Glishorn übernimmt keine Verantwortung für die Resultate des Experiments.</p>
+        <p>Wenn ihr euch selber mal messen wollt, um zu sehen, ob ihr diese Werte übertrefft, empfehlen wir die App
+            <a href="https://skypaw.com/decibel10.html" target="_blank">Decibel X</a>
+            für Android und iOS.
+            Sie ist kostenlos und leicht zu bedienen.</p>
+    </section>
+</div>
+
+<?php include '../templates/footer.php'?>
+</body>
+</html>
