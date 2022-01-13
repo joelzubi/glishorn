aab3dc75e9bd41dae48b638bc950e915cb94bd9a
joelzubi <joel.zubi@gmail.com>
2022-01-13 20:51:46 +0100

    Reorder Start page

--- index.php
+++ index.php
@@ -35,64 +35,26 @@
 <!-- Corona alert -->
 <div id="coronaAlert" class="jumbotron-fluid py-4 mb-3 bg-info text-white">
     <div class="container">
-        <h4 class="my-3">Neustart nach Coronapause</h4>
-        <p>Nach einer langen Zwangspause freuen sich die
-            <abbr title="Musikgesellschaft" style="text-decoration: none;">MG</abbr>&nbsp;Glishorn
-            und die
-            <abbr title="Jugendmusik" style="text-decoration: none;">JM</abbr>&nbsp;Glis,
-            wieder gemeinsam zu musizieren.</p>
-        <p>Nach wie vor gelten Vorsichtsmassnahmen sowie Zertifikatspfilcht an allen Proben und Anlässen.</p>
+        <h4 class="my-3">Die &laquo;Glishorn&raquo; spielt Mozart</h4>
+        <p>
+            Wenn Proben im Orchester nicht möglich sind, musizieren wir halt in Kleingruppen.
+            Unser Konzert im Sommer 2021 fand vor einem virtuellen Publikum statt.
+            9 Ensembles der
+            <abbr title="Musikgesellschaft" style="text-decoration: none;">MG</abbr> Glishorn
+            präsentieren verschiedene Variationen der Musik von
+            <abbr title="Wolfgang" style="text-decoration: none;">W.</abbr>
+            <abbr title="Amadeus" style="text-decoration: none;">A.</abbr> Mozart.
+        </p>
     </div>
 </div>
 
 <!-- News box -->
 <div id="news" class="container">
     <div class="row">
+        <!-- Left part -->
+        <div class="col-6">
 
-        <!-- Konzert Liebespaare 2021 -->
-        <div class="col-lg-6">
-            <div class="card my-2">
-                <img class="card-img-top" src="src/liebespaare/plakat.png" alt="Flyer Winterkonzert 2021"
-                     style="max-height: 300px;">
-                <div class="card-body">
-                    <h4 class="card-title"><time datetime="2021-12-05 17:00">5.&nbsp;Dezember</time>,
-                        Kirche Glis: &laquo;Liebespaare&raquo;&nbsp;&#128149;</h4>
-                    <p class="card-text">
-                        Die <abbr title="Musikgesellschaft" style="text-decoration: none;">MG</abbr> Glishorn
-                        lädt ein zum Konzert in der Wallfahrtskirche.
-                        Unter dem Titel &laquo;Liebespaare&raquo; präsentieren wir musikalisch aufgearbeitete
-                        Liebesgeschichten von Richard Wagner, Camille Saint-Saëns, George Gershwin und anderen.</p>
-                    <a href="articles/liebespaare.php" class="card-link stretched-link">Details</a>
-                </div>
-            </div>
-        </div>
-
-        <!-- Mozart -->
-        <div class="col-lg-6">
-            <div class="card my-2">
-                <img class="card-img-top" src="src/mozart/logo.png" alt="Plakat Glishorn spielt Mozart"
-                     style="max-height: 300px;">
-                <div class="card-body">
-                    <h4 class="card-title">Die &laquo;Glishorn&raquo; spielt Mozart</h4>
-                    <p class="card-text">
-                        Wenn Proben im Orchester nicht möglich sind, musizieren wir halt in Kleingruppen.
-                        Unser Konzert im Sommer 2021 fand vor einem virtuellen Publikum statt.
-                        9 Ensembles der
-                        <abbr title="Musikgesellschaft" style="text-decoration: none;">MG</abbr> Glishorn
-                        präsentieren verschiedene Variationen der Musik von
-                        <abbr title="Wolfgang" style="text-decoration: none;">W.</abbr>
-                        <abbr title="Amadeus" style="text-decoration: none;">A.</abbr> Mozart.
-                    </p>
-                    <a href="articles/mozart.php" class="card-link stretched-link">Jetzt anhören</a>
-                </div>
-            </div>
-        </div>
-
-    </div>
-    <div class="row">
-
-        <!-- Decibel experiment -->
-        <div class="col-lg-6">
+            <!-- Decibel experiment -->
             <div class="card my-2">
                 <img class="card-image-top" src="src/decibel_test/title_picture.png" alt="Lärmbelastung"
                      style="max-height: 300px;">
@@ -106,10 +68,8 @@
                     <a href="articles/decibel_test.php" class="card-link stretched-link">Zum Experiment</a>
                 </div>
             </div>
-        </div>
 
-        <!-- Quarantine music -->
-        <div class="col-lg-6">
+            <!-- Quarantine music -->
             <div class="card my-2">
                 <img class="card-img-top" src="src/quarantine/tromboline.png" alt="Tromboline"
                      style="height: 300px;">
@@ -124,13 +84,12 @@
                     <a href="articles/quarantine.php" class="card-link stretched-link">Videos</a>
                 </div>
             </div>
+
         </div>
+        <!-- Right part -->
+        <div class="col-6">
 
-    </div>
-    <div class="row">
-
-        <!-- JM Glis in Burgdorf -->
-        <div class="col-lg-6">
+            <!-- JM Glis in Burgdorf -->
             <div class="card my-2">
                 <img class="card-img-top" src="src/burgdorf/group.png" alt="Gruppenfoto Jugendmusik Glis"
                      style="max-height: 300px;">
@@ -142,13 +101,13 @@
                         Sie erreichte den 1.&nbsp;Platz in der Kategorie Konzert Harmonie Oberstufe mit 94&nbsp;Punkten
                         sowie den 1.&nbsp;Platz in der Kategorie Parademusik M mit 93&nbsp;Punkten.
                         Hier der Bericht des Präsidenten über das "historische" Wochenende vom
-                        <time datetime="2019-09-21 14:00">21./22.&nbsp;September</time>.
+                        <time datetime="2019-09-21 14:00">21./22.&nbsp;September 2021</time>.
                     </p>
                     <a href="articles/burgdorf.php" class="card-link stretched-link">Zum Bericht</a>
                 </div>
             </div>
-        </div>
 
+        </div>
     </div>
 </div>
 
