86180532083cee5e9a65df6eecfa8e1ac2c1e80e
joelzubi <joel.zubi@gmail.com>
2022-01-14 00:10:43 +0100

    Make Mozart spotlight

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 
     <!-- Custom Stylesheets -->
+    <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/index_header.css">
     <link rel="stylesheet" href="css/footer.css">
 
@@ -38,49 +39,73 @@
 </div>
 
 <!-- Corona alert -->
-<div id="coronaAlert" class="jumbotron-fluid py-4 mb-3 bg-info text-white">
+<div id="coronaAlert" class="jumbotron-fluid py-4 mb-3 bg-warning">
     <div class="container">
-        <h4 class="my-3">Neustart nach Coronapause</h4>
-        <p>Nach einer langen Zwangspause freuen sich die
-            <abbr title="Musikgesellschaft" style="text-decoration: none;">MG</abbr>&nbsp;Glishorn
-            und die
-            <abbr title="Jugendmusik" style="text-decoration: none;">JM</abbr>&nbsp;Glis,
-            wieder gemeinsam zu musizieren.</p>
-        <p>Nach wie vor gelten Vorsichtsmassnahmen sowie Zertifikatspfilcht an allen Proben und Anlässen.</p>
+        <h4 class="my-3">Probebetrieb mit Auflagen</h4>
+        <p>Aufgrund der aktuellen Lage proben wir derzeit im 2G-plus-Modus und halten uns an die
+            aktuellen Vorgaben des Bundes.</p>
+        <p>Nichtsdestotrotz hoffen wir auf eine baldige Normalisierung und freuen uns,
+            schon bald wieder vor Publikum ein Programm zum Besten zu geben.</p>
+    </div>
+</div>
+
+<!-- Mozart -->
+<div class="container my-4">
+    <div class="card hover-box">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-xl-8 position-relative">
+                    <h3>Die &laquo;Glishorn&raquo; spielt Mozart</h3>
+                    <p>
+                        Im Frühjahr 2021 spielten die Mitglieder der
+                        <abbr title="Musikgesellschaft" style="text-decoration: none;">MG</abbr> Glishorn
+                        in Kleingruppen statt im Orchester.
+                    </p>
+                    <p>
+                        9 Ensembles präsentieren verschiedene Variationen der Musik von
+                        <abbr title="Wolfgang" style="text-decoration: none;">W.</abbr>
+                        <abbr title="Amadeus" style="text-decoration: none;">A.</abbr> Mozart.
+                    </p>
+                </div>
+                <div class="col-xl-4">
+                    <img src="src/mozart/logo.jpg" class="img-fluid d-block mx-auto" alt="Mozart">
+                </div>
+            </div>
+            <a href="articles/mozart.php" class="btn btn-primary card-link stretched-link btn-block animated">
+                <span> 🎥 Hören Sie sich das Resultat hier an! </span>
+            </a>
+        </div>
     </div>
 </div>
 
 <!-- News box -->
-<div id="news" class="container">
+<div class="container">
+
     <div class="row">
 
-        <!-- Mozart -->
+        <!-- JM Glis in Burgdorf -->
         <div class="col-lg-6">
-            <div class="card my-2">
-                <img class="card-img-top" src="src/mozart/logo.jpg" alt="Plakat Glishorn spielt Mozart"
+            <div class="card my-2 hover-box">
+                <img class="card-img-top" src="src/burgdorf/group.jpg" alt="Gruppenfoto Jugendmusik Glis"
                      style="max-height: 300px;">
                 <div class="card-body">
-                    <h4 class="card-title">Die &laquo;Glishorn&raquo; spielt Mozart</h4>
+                    <h4 class="card-title">Die JM&nbsp;Glis am Eidgenössischen Jugendmusikfest in Burgdorf&nbsp;🎉</h4>
                     <p class="card-text">
-                        Wenn Proben im Orchester nicht möglich sind, musizieren wir halt in Kleingruppen.
-                        Unser Konzert im Sommer 2021 fand vor einem virtuellen Publikum statt.
-                        9 Ensembles der
-                        <abbr title="Musikgesellschaft" style="text-decoration: none;">MG</abbr> Glishorn
-                        präsentieren verschiedene Variationen der Musik von
-                        <abbr title="Wolfgang" style="text-decoration: none;">W.</abbr>
-                        <abbr title="Amadeus" style="text-decoration: none;">A.</abbr> Mozart.
+                        Am Schweizer Jugendmusikfest in Burgdorf&nbsp;2019 konnte die Jugendmusik&nbsp;Glis so richtig
+                        abräumen.
+                        Sie erreichte den 1.&nbsp;Platz in der Kategorie Konzert Harmonie Oberstufe mit 94&nbsp;Punkten
+                        sowie den 1.&nbsp;Platz in der Kategorie Parademusik M mit 93&nbsp;Punkten.
+                        Hier der Bericht des Präsidenten über das "historische" Wochenende vom
+                        <time datetime="2019-09-21 14:00">21./22.&nbsp;September 2019</time>.
                     </p>
-                    <a href="articles/mozart.php" class="card-link stretched-link">Jetzt anhören</a>
+                    <a href="articles/burgdorf.php" class="card-link stretched-link">Zum Bericht</a>
                 </div>
             </div>
         </div>
 
-    </div>
-    <div class="row">
-
         <!-- Decibel experiment -->
         <div class="col-lg-6">
-            <div class="card my-2">
+            <div class="card my-2 hover-box">
                 <img class="card-image-top" src="src/decibel_test/title_picture.jpg" alt="Lärmbelastung"
                      style="max-height: 300px;">
                 <div class="card-body">
@@ -95,9 +120,12 @@
             </div>
         </div>
 
+    </div>
+    <div class="row">
+
         <!-- Quarantine music -->
         <div class="col-lg-6">
-            <div class="card my-2">
+            <div class="card my-2 hover-box">
                 <img class="card-img-top" src="src/quarantine/tromboline.jpg" alt="Tromboline"
                      style="height: 300px;">
                 <div class="card-body">
@@ -109,29 +137,6 @@
                         Konzerte nicht stattfinden können.
                     </p>
                     <a href="articles/quarantine.php" class="card-link stretched-link">Videos</a>
-                </div>
-            </div>
-        </div>
-
-    </div>
-    <div class="row">
-
-        <!-- JM Glis in Burgdorf -->
-        <div class="col-lg-6">
-            <div class="card my-2">
-                <img class="card-img-top" src="src/burgdorf/group.jpg" alt="Gruppenfoto Jugendmusik Glis"
-                     style="max-height: 300px;">
-                <div class="card-body">
-                    <h4 class="card-title">Die JM&nbsp;Glis am Eidgenössischen Jugendmusikfest in Burgdorf&nbsp;🎉</h4>
-                    <p class="card-text">
-                        Am Schweizer Jugendmusikfest in Burgdorf&nbsp;2019 konnte die Jugendmusik&nbsp;Glis so richtig
-                        abräumen.
-                        Sie erreichte den 1.&nbsp;Platz in der Kategorie Konzert Harmonie Oberstufe mit 94&nbsp;Punkten
-                        sowie den 1.&nbsp;Platz in der Kategorie Parademusik M mit 93&nbsp;Punkten.
-                        Hier der Bericht des Präsidenten über das "historische" Wochenende vom
-                        <time datetime="2019-09-21 14:00">21./22.&nbsp;September</time>.
-                    </p>
-                    <a href="articles/burgdorf.php" class="card-link stretched-link">Zum Bericht</a>
                 </div>
             </div>
         </div>
