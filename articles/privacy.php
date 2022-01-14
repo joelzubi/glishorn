1dd17fbf3278bff815f91a6780c1d50e94c89c1d
joelzubi <joel.zubi@gmail.com>
2022-01-14 03:17:07 +0100

    Add basic Cookie Policy

---
+++ articles/privacy.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1)?>
+<!DOCTYPE html>
+<html lang="de-CH">
+<head>
+    <meta charset="UTF-8">
+    <title>Datenschutz | MG Glishorn</title>
+
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="../src/index_images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../src/index_images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../src/index_images/favicon-16x16.png">
+    <link rel="manifest" href="../src/index_images/site.webmanifest">
+
+    <!-- For responsive design -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap Stylesheet -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+    <!-- Custom Stylesheets -->
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/footer.css">
+
+    <!-- jQuery, Popper JS and Bootstrap Scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+    <!-- Icon Script -->
+    <script src="https://kit.fontawesome.com/f54a4f4510.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+<?php include '../templates/header.php'?>
+<?php include '../templates/navbar_article.php' ?>
+
+<!-- Policy -->
+<div class="container">
+    <h1>Cookie-Richtlinien</h1>
+    <p>Diese Website verwendet möglicherweise Cookies von Drittanbietern, insbesondere Tracking-Cookies,
+        zum Beispiel zur Anzeige von Facebook- oder YouTube-Videos oder für Google Kalender.
+        Dazu bedarf es Ihrer Zustimmung.</p>
+    <p>Ausserdem setzen wir ein Cookie, um die Cookie-Einstellungen zu speichern.
+        Dieses wird auch dann gesetzt, wenn Sie Cookies abgelehnt haben.</p>
+</div>
+
+<?php include '../templates/footer.php'?>
+</body>
+</html>
