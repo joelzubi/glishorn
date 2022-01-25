7e8dc4a5f299365f819627d0ef9b8b00fb7f23a5
joelzubi <joel.zubi@gmail.com>
2022-01-25 13:00:34 +0100

    New calendar, responsive

--- calendar.php
+++ calendar.php
@@ -21,6 +21,28 @@
     <link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="css/footer.css">
 
+    <style>
+        /* Responsive iFrame */
+
+        .responsive-iframe-container {
+            position: relative;
+            padding-bottom: 56.25%;
+            padding-top: 30px;
+            height: 0;
+            overflow: hidden;
+        }
+
+        .responsive-iframe-container iframe,
+        .vresponsive-iframe-container object,
+        .vresponsive-iframe-container embed {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+        }
+    </style>
+
     <!-- jQuery, Popper JS and Bootstrap Scripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
@@ -37,10 +59,15 @@
     <h1>Kalender</h1>
 </div>
 
-<!-- Google Calendar -->
-<div class="container">
-    <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23f2e4b5&ctz=Europe%2FZurich&showTabs=0&showPrint=0&showDate=1&showTz=0&showCalendars=0&showNav=1&showTitle=1&mode=MONTH&hl=de&title=MG%20Glishorn&src=am1xdTltaG9xODFjbTRwYzJhNzc0dGIydWo1MWEzdjZAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%238E24AA"
-            style="border-width:0" width="100%" height="600" frameborder="0" scrolling="no"></iframe>
+<!-- Google Calendar phone -->
+<div class="container responsive-iframe-container d-block d-sm-none">
+    <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Europe%2FZurich&showTitle=1&title=MG%20Glishorn&showTabs=1&showCalendars=0&showTz=0&mode=AGENDA&showNav=1&showDate=0&src=bWdnbGlzaG9ybkBnbWFpbC5jb20&color=%23039BE5"
+            style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+</div>
+<!-- Google Calendar laptop -->
+<div class="container responsive-iframe-container d-none d-sm-block">
+    <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Europe%2FZurich&showTitle=1&title=MG%20Glishorn&showTabs=1&showCalendars=0&showTz=0&mode=MONTH&showNav=1&showDate=1&src=bWdnbGlzaG9ybkBnbWFpbC5jb20&color=%23039BE5"
+            style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 </div>
 
 <?php include 'templates/footer.php'?>
