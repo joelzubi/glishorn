c90193c5f866c86aac6e2a16bebeb48381e3e4cd
joelzubi <joel.zubi@gmail.com>
2022-01-28 11:03:09 +0100

    Add button to Google Calendar

--- calendar.php
+++ calendar.php
@@ -69,6 +69,13 @@
     <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Europe%2FZurich&showTitle=1&title=MG%20Glishorn&showTabs=1&showCalendars=0&showTz=0&mode=MONTH&showNav=1&showDate=1&src=bWdnbGlzaG9ybkBnbWFpbC5jb20&color=%23039BE5"
             style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 </div>
+<!-- Link to full screen -->
+<div class="container mt-2">
+    <a href="https://calendar.google.com/calendar/embed?src=mgglishorn%40gmail.com&ctz=Europe%2FZurich"
+       target="_blank" class="btn btn-primary btn-block">
+        In Google Kalender öffnen
+    </a>
+</div>
 
 <?php include 'templates/footer.php'?>
 </body>
