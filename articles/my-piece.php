2fc0eaff5de1d377ebcbbc90005225cd04e1d3e0
joel.zuber <joel.zuber@protonmail.com>
2023-05-15 13:53:01 +0200

    Big update for BMF 2023

---
+++ articles/my-piece.php
@@ -0,0 +1,201 @@
+<?php declare(strict_types=1)?>
+<!DOCTYPE html>
+<html lang="de-CH">
+<head>
+    <meta charset="UTF-8">
+    <title>MyPiece Mouthpiece</title>
+
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="../src/index_images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../src/index_images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../src/index_images/favicon-16x16.png">
+    <link rel="manifest" href="../src/index_images/site.webmanifest">
+
+    <!-- For responsive design -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap Stylesheet -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+    <!-- Custom Stylesheets -->
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/footer.css">
+
+    <!-- jQuery, Popper JS and Bootstrap Scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+    <!-- Icon Script -->
+    <script src="https://kit.fontawesome.com/f54a4f4510.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+<?php include '../templates/header.php'?>
+<?php include '../templates/navbar_article.php' ?>
+
+<!-- Interview -->
+<div class="container my-2">
+    <h1>
+        <img src="../src/mypiece/mypiece_logo.svg" alt="MyPiece">
+        &ndash; von einem Blechbläser, für Blechbläser
+    </h1>
+
+    <section>
+        <p>
+            Unser Eufonist und Veteran David Lochmatter spielte in der vergangenen Zeit mit verschiedenen farbigen und
+            unförmigen Mundstücken.
+            Neulich mussten sich die Blechbläser unserer Musik nun für dieses Mysterium die Bärte rasieren.
+            Klingt schräg&nbsp;&ndash; am&nbsp;besten erklärt es David selbst.
+        </p>
+    </section>
+    <img src="../src/mypiece/mundstuecke.jpeg" alt="Mundstücke MyPiece" class="img-fluid d-block mx-auto">
+    <section>
+        <p>
+            <b>David, was hat es mit deinen verformten Mundstücken auf sich?</b>
+        </p>
+        <p>
+            Mundstücke für Blechblasinstrumente werden seit Jahrhunderten als Rotationskörper, vorwiegend aus Messing,
+            gedreht.
+            Während mit der Zeit vielfältige Geometrien und Designs entstanden sind, blieb die Randkontur stets flach.
+            Dieser flache Rand ist jedoch einzig der Produktion durch Drehen geschuldet und passt bei Weitem nicht
+            auf jeden Mund.
+            So&nbsp;weisen die Frontzahnreihen meist eine natürliche Krümmung auf und auch ein mehr oder minder ausgeprägter
+            Überbiss ist häufig.
+            Wir&nbsp;&ndash; zu unserem Team gehören neben mir meine Frau Manuela und mein Bruder Thomas&nbsp;&ndash; passen nun das
+            Mundstück an den Mund des Musikers an.
+        </p>
+        <p>
+            <b>Wie hat das angefangen? Wie bist du auf die Idee gekommen, die Mundstücke anzupassen?</b>
+        </p>
+        <p>
+            Während meines Musikstudiums an der Musikhochschule Luzern stellte ich fest dass meine Zahnstellung mich
+            in meinen musikalischen Möglichkeiten begrenzte.
+            Nach meinem Studium experimentierte ich mit rudimentären Werkzeugen wie Feilen und Handschleifmaschinen
+            an meinen Mundstücken nach dem Trial-and-Error-Verfahren.
+            Ich erkannte, dass hier viel Potenzial lag.
+            Die Methode genügte jedoch nicht.
+            Nach und nach wuchs der Wunsch, die Mundform genauer abzumessen.
+            Nach einigen gescheiterten Versuchen mit Gipsabdrücken lieferte ein 3D-Scan verwertbare Daten.
+            Die ersten Scans mussten noch in Zürich oder Visp mit teuren 3D-Scannern getätigt werden.
+            Inzwischen lässt sich das mit Mobiltelefonen und preiswerten Apps in Eigenregie machen.
+            Die Verarbeitung der Scandaten übernahm mein Bruder Thomas.
+            Er&nbsp;schrieb eine Software, welche Mundstücke zeichnen kann, um mithilfe der Scandaten eine angepasste
+            Mundstückrandform zu berechnen.
+        </p>
+        <p>
+            <b>Ihr habt schon früh ein Patent angemeldet. Was waren damals eure Hintergedanken?</b>
+        </p>
+        <p>
+            Nach monatelangem Tüfteln und 79&nbsp;gedruckten Mundstücken mit verschiedenen Parametern fand ich meine für
+            mich optimale Mundstückrandform.
+            Diese eröffnete mir neue, vorher unerreichbar scheinende musikalische Möglichkeiten.
+            Von diesen Erkenntnissen sollen nun auch andere Musiker profitieren können.
+            Deshalb kam schon vor einigen Jahren der Wunsch auf, dieses Produkt zu vermarkten.
+            Ende&nbsp;2021 konnten wir für unsere Erfindung das Patent entgegennehmen.
+        </p>
+        <p>
+            <b>Was sind die Vorteile eines angepassten Mundstücks?</b>
+        </p>
+        <p>
+            Passt das Mundstück nicht gut auf den Mund, kann dies zu zwei Problemen führen:
+            Luft entweicht aus den Mundwinkeln oder es kann Druckstellen auf der Mundpartie geben.
+            Mit unseren Mundstücken kann der Musiker Klangqualität, Tonumfang, Kontrolle, Flexibilität,
+            Dynamik&nbsp;&ndash; vor allem im leisen Bereich &nbsp;&ndash; und Ausdauer verbessern.
+            Natürlich muss aber auch mit unseren Mundstücken noch geübt werden.
+        </p>
+        <p>
+            <b>Was sind deine Erfahrungen bis jetzt? Wie viele Blechbläser nutzen bereits ein angepasstes Mundstück?</b>
+        </p>
+        <p>
+            Ich selbst spiele bereits seit einigen Jahren auf einem Prototyp.
+            Diesen konnte ich in den letzten Jahren weiterentwickeln.
+            Durch das angepasste Mundstück konnte ich meine hohe Lage um einige Töne erweitern und in der tiefen Lage
+            wurde der Klang voller.
+            Die grössten Vorteile spüre ich jedoch in der Kontrolle und Ausdauer.
+            Nur schade, habe ich nicht mehr gleich viel Zeit zum Üben wie während meines Musikstudiums.
+            Unsere Kunststoffprototypen konnten wir bisher vorwiegend mit einigen ausgewählten Schülern und Kollegen
+            testen.
+            Diese Mundstücke konnten von der Oberflächengüte und der Klangqualität jedoch nicht mit herkömmlichen
+            Messingmundstücken mithalten.
+            Interessanterweise konnten viele Probanden durch unsere angepasste Randform kontrollierter, leiser, höher
+            und tiefer spielen.
+            Auch war der Klang in der Höhe offener.
+            Meinem Schwager Pascal Vogel liessen wir eines aus Messing giessen.
+            Er ist begeistert und will nicht mehr zurück.
+            Der bekannte portugiesische Euphoniumsolist Mauro&nbsp;Martins testete einen unserer ersten Plastikprototypen
+            und schrieb:
+            <blockquote>
+                <i>&laquo;Das MyPiece-Mundstück kann bei der Flexibilität und dem Tonumfang wirklich helfen.
+                    Das Spielgefühl ist etwas anders, aber sehr positiv.&raquo;</i>
+            </blockquote>
+        </p>
+        <p>
+            <b>Wie läuft die Herstellung eines angepassten Mundstücks ab?</b>
+        </p>
+        <p>
+            Wir planen zwei Produktlinien.
+            Beide können aus Messing und Kunstharz hergestellt werden.
+            Bei <i>MyPiece&nbsp;basic</i> bieten wir Mundstücke mit Standardkrümmungen an, welche für einen Grossteil der Kunden
+            einen möglichst grossen Nutzen bringen sollen.
+            Hierfür braucht es keinen 3D-Scan.
+            Der 3D-Scan wird allerdings für das <i>MyPiece&nbsp;professional</i> benötigt.
+            Das <i>MyPiece&nbsp;professional</i> ist optimal auf den Kunden angepasst, sodass dieser sein volles Potential
+            ausschöpfen kann.
+            Nach dem 3D-Scan drucken wir dem Kunden drei individuell angepasste Mundstücke aus biokompatiblem
+            Kunstharz mit verschiedenen Glättungsparametern.
+            Der Kunde kann diese in der Folge testen und anschliessend bei uns sein persönliches
+            <i>MyPiece-professional</i>-Mundstück aus Messing anfertigen lassen.
+            Während der 3D-Druck von Einzelanfertigungen wie unseren Mundstücken technisch sehr einfach ist, ist es
+            eine grosse Herausforderung, Einzelanfertigungen zu einem akzeptablen Preis aus Messing zu drehen und
+            fräsen zu lassen.
+            An&nbsp;der technischen Lösung für dieses Problem grübelten wir während der letzten vier Jahre, der Durchbruch
+            gelang uns dann endlich diesen Frühling.
+        </p>
+        <p>
+            <b>3D-Scan und Druck, verschiedene Materialien&nbsp;etc. Das hört sich technisch herausfordernd an.
+            Wie hast du dir das alles beigebracht?</b>
+        </p>
+        <p>
+            Im&nbsp;Bereich des 3D-Modellierens und 3D-Drucks gibt es im Internet zahlreiche Tipps und Lernvideos.
+            Dann geht vieles natürlich über Ausprobieren und Nachlesen.
+            Beim Programmieren muss ich jedoch kapitulieren.
+            Da&nbsp;kommt mein Bruder Thomas ins Spiel.
+        </p>
+        <p>
+            <b>Was sind eure Ziele für MyPiece? Wo seht ihr MyPiece in 10&nbsp;Jahren?</b>
+        </p>
+        <p>
+            Unser Ziel ist es, im Herbst&nbsp;2023 unsere Entwicklungsphase abzuschliessen un in einer nächsten Phase
+            schweizweit und dann nach und nach weltweit liefern zu können.
+            Da&nbsp;unser Produkt sehr innovativ ist, ist es sehr schwierig abzuschätzen, wie gross unser Markt sein
+            wird und wo wir in 10&nbsp;Jahren stehen.
+            Wir lassen uns überraschen.
+        </p>
+        <p>
+            <b>Ihr habt eine Studie gestartet. Was sind eure Ziele für diese Studie?</b>
+        </p>
+        <p>
+            Um unsere Idee weiterzuentwickeln, haben wir eine Scan- und Testkampagne lanciert.
+            Daran nehmen neben der <abbr title="Musikgesellschaft">MG</abbr>&nbsp;Glishorn fünf weitere Vereine aus dem
+            Wallis teil.
+            Ausserdem konnten wir eine Eufoniumklasse der Musikhochschule Luzern als Forschungspartner gewinnen.
+            Wir wollen besser verstehen, inwieweit die Zahnstellung mit den musikalischen Möglichkeiten bei
+            Blechbläsern zusammenhängt und ob ein angepasstes Mundstück die musikalischen Möglichkeiten erweitert,
+            und wenn ja, bei welchen Instrumenten und mit welchen Parametern.
+            Bei der Erstellung der angepassten Mundstücke mithilfe der Scans gibt es für uns viele verschiedene
+            Parameter zu bestimmen.
+            Mit den Daten aus der Scan- und Testkampagne wollen wir die besten Parameter für unsere Kunden herausfinden.
+            Gleichzeitig sollen uns die Daten helfen, die besten <i>MyPiece-basic-</i>Mundstücke zu erstellen.
+            Ausserdem möchten wir so den Markt analysieren.
+        </p>
+        <img src="../src/mypiece/David.jpg" alt="Porträt David Lochmatter" class="img-fluid d-block mx-auto">
+        <p>
+            Mehr zu MyPiece könnt ihr auf <a href="https://www.mypiece.me/">www.mypiece.me</a> lesen.
+        </p>
+    </section>
+</div>
+
+<?php include '../templates/footer.php'?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -4,35 +4,177 @@ include 'page_builder.php';
 
 $page = new Page();
 $page->mainpage();
-$page->dont_show_title();
+$page->set_title('Musikgesellschaft Glishorn');
 $page->set_tab_title('MG Glishorn');
 
 $page->begin();
 ?>
 
-<!-- BMF 2023 -->
+<?php if (time() < mktime(16, 15, 0, 5, 21, 2023)): ?>
+
+<!-- BMF Friday Video -->
+<?php if (time() < mktime(2, 0, 0, 5, 20, 2023)): ?>
+    <div class="container">
+        <video width="100%" controls style="max-height: 700px">
+            <source src="src/bmf23/BMF_P2_V2.mp4" type="video/mp4">
+        </video>
+    </div>
+<?php endif ?>
+
+<!-- BMF 2023 - General -->
 <div class="container">
-    <h1>Musikgesellschaft Glis</h1>
-
     <p>
-        Am Auffahrtswochenende 2023 dürfen wir die Musikfamilie des Bezirks Brig
-        nach 9 Jahren wieder einmal in Glis zum Bezirksmusikfest begrüssen.
-        Neben den 9 Vereinen des Bezirks freut es uns ganz besonders,
-        auch unsere Freunde der Trachtenkapelle Malta aus Kärnten in Österreich bei uns begrüssen zu dürfen.
-        Ein OK unter der Leitung von Stadtrat Daniel Studer hat sich bereits an die Arbeit gemacht,
-        um uns und Glis von der besten Seite präsentieren zu können.
-        Wir hoffen, Sie alle an diesem Wochenende bei uns auf dem Dorfplatz zum Festakt,
-        entlang der Gliserallee beim Einmarsch oder am Freitag und Samstag im Glsergrund
-        auf dem Festareal begrüssen zu dürfen.
+        Die Musikgesellschaft «Glishorn»&nbsp;Glis hat die Ehre, das 83.&nbsp;Verbandsmusikfest zu organisieren und als
+        Gastgeberin die Musikvereine des Bezirks&nbsp;Brig am <time datetime="2023-05-19">19.</time> und
+        <time datetime="2023-05-20">20.&nbsp;Mai</time> in&nbsp;Glis willkommen zu&nbsp;heissen.
     </p>
+    <p>
+        Die Musikantinnen und Musikanten der «Glishorn» sowie die Mitglieder des Organisationskomitees heissen Sie
+        auf dem Festplatz gegenüber der OS&nbsp;Brig-Süd herzlich willkommen – wir freuen uns auf ein gelungenes
+        Musikfest! &#x1F3B5;&#x1F389;
+    </p>
+</div>
 
+<!-- Big logo -->
+<div class="container">
     <img src="src/bmf23/BMF%202023%20Logo%20farbig.png" alt="Bezirksmusikfest Glis"
-         class="img-fluid mx-auto d-block" style="width: 580px;">
+         class="img-fluid mx-auto d-block" style="width: 600px;">
+</div>
+
+<!-- Friday program ad -->
+<?php if (time() < mktime(4, 0, 0, 5, 20, 2023)): ?>
+    <div class="container my-3 px-0">
+        <img src="src/bmf23/Plakat_Freitag.png" alt="Freitag, 19. Mai 2023
+            18:00 Uhr VIP-Apéro für Sponsoren und Ehrenmitglieder
+            19:00 Uhr Konzert &laquo;Modern Wheat Beat &raquo;
+            21:00 Uhr Konzert &laquo;Kalpetrane&raquo;
+            23:00 Uhr Party mit &laquo;DJ Cale&raquo;"
+                 class="img-fluid" style="white-space: pre;" >
+    </div>
+<?php endif ?>
+
+<!-- Saturday program ad -->
+<div class="container my-3 px-0">
+    <img src="src/bmf23/Plakat_Samstag.png" alt="Samstag, 20. Mai 2023
+        14:30 Uhr Festakt auf dem Dorfplatz
+        15:15 Uhr Festumzug
+        16:20 Uhr Konzerte der Musikvereine des Bezirks Brig im Festzelt
+        17:00 Uhr Ehrungen im Festzelt
+        19:15 Uhr Gala-Konzert Trachtenkapelle Malta Kärnten
+        20:50 Uhr Unterhaltung mit &laquo;Z'Hansrüedi&raquo;"
+         class="img-fluid" style="white-space: pre;">
+</div>
+
+<!-- Dinner... -->
+<?php if (mktime(4, 0, 0, 5, 20, 2023) < time()
+        && time() < mktime(13, 30, 0, 5, 20, 2023)) {
+    $program = new ConcertProgram('Mittagessen im Festzelt');
+    $program->add_line_2('Glis', '11:30 Uhr');
+    $program->add_line_2('Malta', '11:30 Uhr');
+    $program->add_line_2('Simplon-Dorf', '11:30 Uhr');
+    $program->add_line_2('Ried-Brig', '11:45 Uhr');
+    $program->add_line_2('Termen', '11:45 Uhr');
+    $program->add_line_2('Mörel-Filet', '12:00 Uhr');
+    $program->add_line_2('Eggerberg', '12:00 Uhr');
+    $program->add_line_2('Mund', '12:15 Uhr');
+    $program->add_line_2('Naters', '12:15 Uhr');
+    $program->add_line_2('Brig', '12:30 Uhr');
+    $program->print_table();
+}
+?>
+
+<!-- Situation plan -->
+<img src="src/bmf23/standorte.jpg" alt="Situationsplan" class="img-fluid">
+
+<!-- BMF 2023 - Details -->
+<div class="container">
+    <!-- Marching Order -->
+    <?php $program = new ConcertProgram('Reihenfolge Einmarsch');
+    $program->add_line_1('Musikgesellschaft &laquo;Glishorn&raquo; Glis');
+    $program->add_line_1('Trachtenkapelle Malta');
+    $program->add_line_1('Musikgesellschaft &laquo;Belalp&raquo; Naters');
+    $program->add_line_1('Musikgesellschaft &laquo;Saflisch&raquo; Termen');
+    $program->add_line_1('Musikgesellschaft &laquo;Findneralp&raquo; Eggerberg');
+    $program->add_line_1('Musikgesellschaft &laquo;Eintracht&raquo; Mörel-Filet');
+    $program->add_line_1('Musikgesellschaft &laquo;Bleiken&raquo; Simplon-Dorf');
+    $program->add_line_1('Musikgesellschaft &laquo;Simplon&raquo; Ried-Brig');
+    $program->add_line_1('Musikgesellschaft &laquo;Bryscheralp&raquo; Mund');
+    $program->add_line_1('Blasorchester Stadtmusik &laquo;Saltina&raquo; Brig');
+    $program->print_table();
+    ?>
+    
+    <!-- Tutti plan -->
+    <?php print_flyer('Aufstellung Gesamtspiel', true, 'src/bmf23/gesamtspiel.jpg'); ?>
+    
+    <!-- Concert order -->
+    <?php $program = new ConcertProgram('Reihenfolge Konzerte');
+    $program->add_line_1('Musikgesellschaft &laquo;Belalp&raquo; Naters');
+    $program->add_line_1('Musikgesellschaft &laquo;Saflisch&raquo; Termen');
+    $program->add_line_1('Musikgesellschaft &laquo;Findneralp&raquo; Eggerberg');
+    $program->add_line_1('Musikgesellschaft &laquo;Eintracht&raquo; Mörel-Filet');
+    $program->add_line_1('Musikgesellschaft &laquo;Bleiken&raquo; Simplon-Dorf');
+    $program->add_line_1('Musikgesellschaft &laquo;Simplon&raquo; Ried-Brig');
+    $program->add_line_1('Musikgesellschaft &laquo;Bryscheralp&raquo; Mund');
+    $program->add_line_1('Blasorchester Stadtmusik &laquo;Saltina&raquo; Brig');
+    $program->print_table();
+    ?>
+</div>
+
+<!-- Sponsoren -->
+<div class="container">
+    <img src="src/bmf23/sponsoren_bmf.jpeg" alt="Sponsoren Bezirksmusikfest 2023" class="img-fluid"
+    style="width: 100%">
+</div>
+
+<?php elseif (time() < mktime(19, 50, 0, 5, 28, 2023)): ?>
+
+<!-- OMF -->
+<div class="container">
+    <img src="src/bmf23/BMF%202023%20Logo%20farbig.png" alt="Bezirksmusikfest Glis"
+         class="img-fluid mx-auto d-block" style="width: 600px;">
+</div>
+<div class="container">
+    <p>
+        Wir danken allen Besuchern des Bezirksmusikfests Brig&nbsp;2023 in Glis für zwei
+        unvergessliche Tage im Kreise der Musikfamilie.
+    </p>
+    <p>
+        Unser nächstes Highlight im Vereinsjahr ist das Oberwalliser Musikfest in Ried-Brig.
+        Wir würden uns freuen, Sie an der Paradestrecke, im Saal oder im Festzelt zu begrüssen.
+        Unsere Spielzeiten sind wie folgt:
+    </p>
+    <?php $program = new ConcertProgram('27. Mai 2023 in Ried-Brig');
+    $program->add_line_3('12:00&nbsp;Uhr', 'A&nbsp;Pedra da Serpe', 'Turnhalle &laquo;Brigerbärg&raquo;');
+    $program->add_line_3('18:09&nbsp;Uhr', 'Infanterie-Regiment 31', 'Parademusikstrecke');
+    $program->print_table();
+    ?>
+</div>
+<img src="src/bmf23/Oberwalliser-Musikfest-2023-2048x1292.jpg" alt="Plakat Oberwalliser Musikfest in Ried-Brig"
+     class="img-fluid mb-3">
+
+<?php endif ?>
+
+<hr>
+
+<!-- My Piece -->
+<div class="container mt-4">
+    <div class="card my-2 hover-box">
+        <img class="card-img-top" src="src/mypiece/tech.jpeg" alt="MyPiece Mundstück Symbolbild">
+        <div class="card-body">
+            <h4 class="card-title">&laquo;MyPiece&raquo;&nbsp;&dash; von einem Blechbläser, für Blechbläser 🎺</h4>
+            <p class="card-text">
+                Unser Eufonist David Lochmatter spielte in letzter Zeit mit verschiedenen farbigen und unförmigen
+                Mundstücken.
+                Hier erfahrt ihr, was es damit auf sich hat und warum ein Start-up aus der Blasmusikszene im Finale
+                des Prix&nbsp;Créateurs&nbsp;2023 der WKB ist.
+            </p>
+            <a href="articles/my-piece.php" class="card-link stretched-link">Mehr erfahren</a>
+        </div>
+    </div>
 </div>
 
 <!-- News box -->
-<div class="container">
-
+<div class="container mt-5">
     <div class="row">
 
         <!-- JM Glis in Burgdorf -->
@@ -68,27 +210,6 @@ $page->begin();
                         Wir haben den Selbsttest gemacht und Erstaunliches festgestellt.
                     </p>
                     <a href="articles/decibel_test.php" class="card-link stretched-link">Zum Experiment</a>
-                </div>
-            </div>
-        </div>
-
-    </div>
-    <div class="row">
-
-        <!-- Quarantine music -->
-        <div class="col-lg-6">
-            <div class="card my-2 hover-box">
-                <img class="card-img-top" src="src/quarantine/tromboline.jpg" alt="Tromboline"
-                     style="height: 300px;">
-                <div class="card-body">
-                    <h4 class="card-title">Musik trotz Corona</h4>
-                    <p class="card-text">
-                        Während einer Pandemie zu musizieren erfordert Kreativität.
-                        Daran fehlt es in der Oberwalliser Musikszene aber nicht.
-                        Verschiedene Formationen bereichern das Leben der Menschen, auch wenn Proben und
-                        Konzerte nicht stattfinden können.
-                    </p>
-                    <a href="articles/quarantine.php" class="card-link stretched-link">Videos</a>
                 </div>
             </div>
         </div>
